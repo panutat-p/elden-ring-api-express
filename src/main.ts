c264c3662a12b5255060936f195c24b2c7f1e3c0
panutat-p <pp.panutat@gmail.com>
2023-04-08 19:46:59 +0700

    feat: config TS to support for hasOwn

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,16 @@ import { getAnime } from './anime.js';
 getAnime()
   .then((r) => console.log(r))
   .catch((e) => console.log(`🟥 ${JSON.stringify(e)}`));
+
+const object1 = {
+  prop: 'exists',
+};
+
+console.log(Object.hasOwn(object1, 'prop'));
+// true
+
+console.log(Object.hasOwn(object1, 'toString'));
+// false
+
+console.log(Object.hasOwn(object1, 'undeclaredPropertyValue'));
+// false
