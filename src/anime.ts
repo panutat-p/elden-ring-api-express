76dcb3515e489d0f2c127f2da19d646e8bbb53f7
panutat-p <pp.panutat@gmail.com>
2023-04-08 19:47:10 +0700

    fix: wrong verb

--- src/anime.ts
+++ src/anime.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError, AxiosRequestConfig } from 'axios';
 
 export async function getAnime(): Promise<any> {
   const config: AxiosRequestConfig = {
-    method: 'POST',
+    method: 'GET',
     url: 'https://animechan.vercel.app/api/random',
   };
 
